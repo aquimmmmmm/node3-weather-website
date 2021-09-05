e913166d185e3ec3e730c0f1962834c086add95d
unknown <you@example.com>
2021-09-05 21:50:03 +0800

    Adding some text

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -9,8 +9,7 @@ const forecast = (latitude, longitude, callback) => {
         }else if(body.error){
             callback('Unable to find location', undefined)
         }else{
-            console.log(body.current)
-            callback(undefined, body.current.weather_descriptions[0] + '. It is currently ' + body.current.temperature + ' degress out. It feels like ' + body.current.feelslike + ' degress out. The humidity is ' + body.current.humidity + '%')
+            callback(undefined, body.current.weather_descriptions[0] + '. It is currently ' + body.current.temperature + ' degress out. It feels like ' + body.current.feelslike + ' degress out. The humidity is ' + body.current.humidity + '%.')
         }
     })
 }
